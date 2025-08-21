1c7399e584b5e01f4bb42899dd6b67a47c82ab2e
AlexeyVolovinin <alekseivolovinin@gmail.com>
2025-08-21 15:35:57 +0300

    first commit

--- commit.sh
+++ commit.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/AlexeyVolovinin/21pAYJG-kRV1-ic.git
+git remote add origin https://github.com/AlexeyVolovinin/T5Gv5.git
 git push -u origin main
